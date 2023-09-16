57247121b84040d598045eecdd8dbf99883b4252
davy52 <dkliminskip@gmail.com>
2023-09-16 19:34:11 +0200

    initial repo commit

---
+++ Makefiles/mk_header_files.mk
@@ -0,0 +1,7 @@
+# header files dir
+HEADER_DIR=""
+
+# add files here
+HEADER_FILES+="../include/"
+
+# HEADER_FILES+=HEADER_DIR+""
---
+++ Makefiles/mk_src_files.mk
@@ -0,0 +1,9 @@
+SRC_FILES="
+
+# src files dir
+SRC_DIR="../src/"
+
+# add files here
+SRC_FILES+=$(SRC_DIR)+"main.c"
+
+# SRC_FILES+=$(SRC_DIR)+""
