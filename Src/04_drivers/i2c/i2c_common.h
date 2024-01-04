95707173bcc210e12603aec6bbe01f2e16ef2770
davy52 <dkliminskip@gmail.com>
2024-01-05 00:12:54 +0100

    i2c registers and clock setup function

--- Src/04_drivers/i2c/i2c_common.h
+++ Src/04_drivers/i2c/i2c_common.h
@@ -2,15 +2,17 @@
 #define I2C_COMMON_H
 
 typedef enum {
-    I2C_OK,
-    I2C_NOT_OK,
-    I2C_BUSY
+    I2C_ERR_OK = 0x01,
+    I2C_ERR_BUSY,
+
+    I2C_ERR_NOT_OK = 0xFF
 } i2c_error_t;
 
 
 
 #define SCL A5
 #define SDA A4
+
 
 //TODO SCL_Freq = F_CPU/(16 + 2(TWBR) * Prescaler)
 
