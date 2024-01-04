94084bf6491654611ae359601c8803b960370d64
davy52 <dkliminskip@gmail.com>
2024-01-05 00:12:54 +0100

    i2c registers and clock setup function

---
+++ Src/04_drivers/i2c/i2c_internal_types.h
@@ -0,0 +1,58 @@
+#ifndef I2C_INTERNAL_TYPES_H
+#define I2C_INTERNAL_TYPES_H
+
+#include <stdint.h>
+
+#define __packed    __attribute__((packed))
+
+// TWI Bit Rate Register
+typedef uint8_t i2c_TWBR_t;
+
+// TWI Control Register
+typedef union __i2c_TWCR {
+    uint8_t reg;
+    struct {
+        uint8_t int_en          : 1;
+        uint8_t                 : 1;
+        uint8_t i2c_en          : 1;
+        uint8_t write_col_flag  : 1;
+        uint8_t write_stop_bit  : 1;
+        uint8_t write_start_bit : 1;
+        uint8_t ack_en          : 1;
+        uint8_t int_flag        : 1;
+    } __packed;
+} i2c_TWCR_t;
+
+// TWI Status Register
+typedef union __i2c_TWSR {
+    uint8_t reg;
+    struct {
+        uint8_t prescaler   : 2;
+        uint8_t             : 1;
+        uint8_t status      : 5;
+    } __packed;
+} i2c_TWSR_t;
+
+// TWI Data Register
+typedef uint8_t i2c_TWDR_t
+
+// TWI Slave Address Register
+typedef union __i2c_TWAR {
+    uint8_t reg;
+    struct {
+        uint8_t addr                : 7;
+        uint8_t gen_cal_recognision : 1;
+    } __packed;
+} i2c_TWAR_t;
+
+// TWI Slave Address Mask
+typedef union __i2c_TWAMR {
+    uint8_t reg;
+    struct {
+        uint8_t         : 1;
+        uint8_t mask    : 7;
+    } __packed;
+} i2c_TWAMR_t;
+
+
+#endif /* I2C_INTERNAL_TYPES_H */
--- Src/04_drivers/i2c/i2c_master.c
+++ Src/04_drivers/i2c/i2c_master.c
@@ -1,4 +1,54 @@
 #include "i2c_master.h"
+#include "i2c_internal_types.h"
+
+#define __at(addr)  __attribute__((address (addr)))
+
+// TWI Bit Rade Register
+volatile static i2c_TWBR_t i2c_bitrate __at(0xB8);
+
+// TWI Control Register
+volatile static i2c_TWCR_t i2c_control __at(0xBC);
+
+// TWI Status Register 
+volatile static i2c_TWSR_t i2c_status __at(0xB9);
+
+// TWI Data Register
+volatile static i2c_TWDR_t i2c_data __at(0xBB);
+
+
+// static functions
+static i2c_error_t i2c_clockSetup(uint32_t cpu_clock, uint32_t scl_freq, uint8_t* bit_rate, uint8_t* prescaler)
+{
+    i2c_error_t ret_val = I2C_ERR_NOT_OK;
+    
+    const float clock_err = 0.05;
+    uint32_t scl_real = 0;
+    uint8_t temp_bitrate = 0;
+    uint8_t temp_prescaler = 0;
+
+    // prescalers               4       16      64
+    const uint8_t pre_arr[] = { 0x01,   0x02,   0x03};
+    
+    // try each prescaler
+    for(uint8_t i = 0; i < 3; i++){
+        temp_prescaler = pre_arr[i];
+        temp_bitrate = (uint32_t)((((cpu_clock * temp_prescaler) / scl_freq) - 16) << 1);
+        scl_real = (uint32_t)(cpu_clock/(16 + 2 * temp_bitrate * temp_prescaler));
+        // if calculated clock if within limits return succes else check the next prescaler
+        if(((scl_real - scl_freq) & 0x7F) <= scl_freq * clock_err){
+            (*prescaler) = temp_prescaler;
+            (*bit_rate) = temp_bitrate;
+            ret_val = I2C_ERR_OK;
+            return ret_val;
+        }
+    }
+
+    // return I2C_ERR_NOT_OK if values not found
+    return ret_val;
+}
+
+
+
 
 void i2c_master_init(i2c_settings_t settings)
 {
@@ -34,7 +84,6 @@ void i2c_master_sendTest()
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
     
 }
-/*
 void i2c_master_sendData(uint8_t *data, uint8_t data_size)
 {
 }
@@ -61,4 +110,3 @@ i2c_error_t i2c_master_readData(uint8_t *data, uint8_t data_size)
 ISR(TWI_vect)
 {
 }
-*/
